5c14b0ffeabc2bff1e9ec5abda4bafdec5eecb62
Rajshah7 <63506258+Rajshah7@users.noreply.github.com>
2026-05-12 13:29:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,34 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+	let prices = document.querySelectorAll(".price");
+
+	let total = Array.from(prices).reduce((acc,price) => {
+		return acc + Number(price.textContent);
+	},0);
+
+	const oldRow =
+        document.getElementById("total-row");
+
+    if (oldRow) {
+        oldRow.remove();
+    }
+
+	const tr = document.createElement('tr');
+	tr.id = "total-row";
+
+	const td = document.createElement("td");
+
+    td.colSpan = 2;
+
+	td.textContent =
+        `Total Price: ${total}`;
+
+	tr.appendChild(td);
+
+	document
+        .querySelector("table")
+        .appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
